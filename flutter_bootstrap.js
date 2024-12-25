a96c35df34f0aeed1f3b9c87da4ec58e1c2a81c7
Mohammed <mohamedasharf55@gmail.com>
2024-12-25 15:09:38 +0200

    Add Loader

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3448171731"
+    serviceWorkerVersion: "2800567089"
   }
 });
