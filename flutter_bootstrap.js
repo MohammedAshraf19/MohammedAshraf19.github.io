18139b488892069398d1ba80e197a2217fff3a58
Mohammed <mohamedasharf55@gmail.com>
2024-12-25 15:21:38 +0200

    Solve Loader

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2800567089"
+    serviceWorkerVersion: "1936730680"
   }
 });
